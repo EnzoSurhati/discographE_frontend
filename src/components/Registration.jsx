654e681211f95995461a1a4c2c93791b3db867fc
Johnahan-web <j.tuckerjrmt@gmail.com>
2025-02-11 16:47:55 -0500

    finished and debugged the register page

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -8,9 +8,12 @@ const Registration = () => {
   
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
 
   
-  const handleSubmit = async(e,email,password) => {
+  const handleSubmit = async(e,email,lastname,firstname,password) => {
     e.preventDefault();
     try {
         const addUser = await fetch("http://localhost.com:3000/api/register",{
@@ -18,7 +21,7 @@ const Registration = () => {
             headers: {
                 "Content-Type":"application/json"
             },
-            body: JSON.stringify({email,password})
+            body: JSON.stringify({email,lastname,firstname,password})
         });
         console.log(addUser)
     } catch (error) {
@@ -30,7 +33,7 @@ const Registration = () => {
   };
 
   return (
-    <form onSubmit={(e)=>handleSubmit(e,email,password)}>
+    <form onSubmit={(e)=>handleSubmit(e,email,lastName,firstName,password)}>
       <input placeholder='Email'
         label="Email"
         type="text"
@@ -44,7 +47,20 @@ const Registration = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-     <button onClick={(e)=>handleSubmit(e,email,password)}>Register</button>
+      <input placeholder='lastname'
+        label="lastname"
+
+        type="lastname"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        />
+<input placeholder='firstname'
+        label="firstname"
+
+        type="firstname"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}/>
+     <button onClick={(e)=>handleSubmit(e,email,lastName,firstName,password)}>Register</button>
     </form>
   );
 };
