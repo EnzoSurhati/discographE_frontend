cce877894476c0daaced2cae8d2266548ff64175
EnzoSur <enzosurhati@gmail.com>
2025-02-28 20:28:35 -0500

    vite config host allowence

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+  },
+  preview: {
+    allowedHosts: ['hi-fi-whd3.onrender.com'],
+  },
   plugins: [react()],
-})
+});
